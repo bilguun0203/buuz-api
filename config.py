ef0807e74225bc1df2084633ce51f059b7bad960
Bilguun Ochirbat <bilguun0203@gmail.com>
2023-02-16 20:09:12 +0800

    feat: set config using environment variables

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
+import os
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    model_path: str = "./assets/model.onnx"
-    confidence_threshold: float = 0.5
-    overlap_threshold: float = 0.5
-    file_size_limit: int = 10485760
+    model_path: str = os.getenv("MODEL_PATH", "./assets/model.onnx")
+    confidence_threshold: float = os.getenv("CONFIDENCE_THRESHOLD", 0.5)
+    overlap_threshold: float = os.getenv("OVERLAP_THRESHOLD", 0.5)
+    file_size_limit: int = os.getenv("FILE_SIZE_LIMIT", 10485760)
     
     class Config:
         env_file = ".env"
